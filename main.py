852f3e3e3a183aac96407e8adf62c9dcb50e68a3
danilevich23555 <danilevich23555@yandex.ru>
2021-10-22 14:27:56 +0300

    add 1 check and 2 food name

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from pprint import pprint
 import os
 path = os.path.join(os.getcwd(), 'file_test.txt')
 
-
-
 with open(path) as data:
     cook_book = {}
     for string_all in data:
@@ -11,27 +9,31 @@ with open(path) as data:
         counter = int(data.readline().strip())
         temp = []
         for string in range(counter):
-            ingredient_name, quantity, measure = data.readline().strip().split(' | ')
-            temp.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+            product_name, quantity, measure = data.readline().strip().split(' | ')
+            temp.append({'product_name': product_name, 'quantity': quantity, 'measure': measure})
         cook_book[name_food] = temp
         data.readline().strip()
-# pprint(cook_book)
 
 # ________________function___________________
-pprint(int(cook_book['Фахитос'][0]['quantity']))
-
-
 def get_shop_list_by_dishes(dishes, person_count):
-    ingredient_check = {}
+    check = {}
     for dish in dishes:
         for count in range(len(cook_book.keys())):
-            if dish == (list(cook_book.keys()))[count]:
+            if dish == (list(cook_book.keys()))[count] and dishes.count(dish) == 1:
                 for string1 in range(len(cook_book[dish])):
-                    ingredient_check[cook_book[dish][string1]['ingredient_name']] = {'measure': cook_book[dish][string1]['measure'], 'quantity': int(cook_book[dish][string1]['quantity'])*person_count}
-    return ingredient_check
+                    check[cook_book[dish][string1]['product_name']] = {'measure': cook_book[dish][string1]
+                    ['measure'], 'quantity': int(cook_book[dish][string1]['quantity'])*person_count}
+            elif dish == (list(cook_book.keys()))[count] and dishes.count(dish) > 1:
+                for string1 in range(len(cook_book[dish])):
+                    check[cook_book[dish][string1]['product_name']] = {'measure': cook_book[dish][string1]
+                    ['measure'],'quantity': int(cook_book[dish][string1]['quantity']) * dishes.count(dish)*person_count}
+    return check
+# _________________________________function result___________________________________
+pprint(get_shop_list_by_dishes(['Омлет'], 2))
+pprint(get_shop_list_by_dishes(['Омлет', 'Омлет'], 1))
 
 
-pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет', 'Фахитос'], 2))
+
 
 
 
